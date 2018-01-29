89f0cb0c702d5b905a858d2dff587e9675dfca35
xuexingxing <945050646@qq.com>
2018-01-29 20:26:30 +0800

    init files

---
+++ Koolearn/items.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class KoolearnItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    # pass
+    sourse = scrapy.Field() # 题目来源
+    grade = scrapy.Field() # 年级: 高中,初中
+    subject = scrapy.Field() # 学科：语文,数学...
+    s_type = scrapy.Field() # 试卷
+    question_num = scrapy.Field() # 单选,多选
+    question_type = scrapy.Field() # 题目类型
+    difficulty_level = scrapy.Field() # 难度等级
+    question_value = scrapy.Field() # 问题内容
+    answer_value = scrapy.Field() # 问题答案
+    answer_extend_link = scrapy.Field() # 问题拓展链接
+    answer_explain = scrapy.Field() # 答案解析
+
+    
---
+++ Koolearn/spiders/koolearn.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from Koolearn.items import KoolearnItem
+
+class KoolearnSpider(scrapy.Spider):
+    name = 'koolearn'
+    # allowed_domains = ['']
+    start_urls = ['http://flow.koolearn.com/shiti/list-1-1-0-1.html?yyue=a21bo.50862.201879']
+
+    base_url = "http://flow.koolearn.com"
+    end_url = "?yyue=a21bo.50862.201879"
+
+    def parse(self, response):
+        # 年级
+        # Item['grade'] = response.xpath("//div[@class='filter-item g-clear'][1]//li[@class='active']/a/text())").extract()[0]
+        
+        # 科目
+        # Item['subject'] = response.xpath("//div[@class='filter-item g-clear'][2]//li[@class='active']/a/text()").extract()[0]
+
+        # 问题查看答案列表链接
+        watch_answer_urls = response.xpath("//a[text()='查看答案']/@href").extract()
+        
+        for url in watch_answer_urls:
+            answer_url = self.base_url + url + self.end_url
+            yield scrapy.Request(answer_url, self.answer_parse)
+
+        # 下一页
+        if len( response.xpath("//a[@class='next']/@href")):
+            next_url = self.base_url + response.xpath("//a[@class='next']/@href").extract()[0]
+            yield scrapy.Request(next_url, self.parse)
+        
+        return
+
+    def answer_parse(self, response):
+        item = KoolearnItem()
+        
+        # xpath解析response
+        if len(response.xpath("/html/body/div[1]/div[2]/div[1]/a[1]/text()")) == 0:
+            item['sourse'] = ""
+        item['sourse'] = response.xpath("/html/body/div[1]/div[2]/div[1]/a[1]/text()").extract()[0]
+        
+        if len(response.xpath("/html/body/div[1]/div[2]/div[1]/a[2]/text()"))  == 0:
+            item['grade'] = ""
+        item['grade'] = response.xpath("/html/body/div[1]/div[2]/div[1]/a[2]/text()").extract()[0][0:2]
+        
+        if len(response.xpath("/html/body/div[1]/div[2]/div[1]/a[2]/text()"))  == 0:
+            item['subject'] = ""
+        item['subject'] = response.xpath("/html/body/div[1]/div[2]/div[1]/a[2]/text()").extract()[0][2:4]
+        
+        if len(response.xpath("/html/body/div[1]/div[2]/div[1]/a[2]/text()"))  == 0:
+            item['s_type'] = ""
+        item['s_type'] = response.xpath("/html/body/div[1]/div[2]/div[1]/a[2]/text()").extract()[0][4:6]
+        
+        if len(response.xpath("/html/body/div[1]/div[2]/div[2]/div[1]/div[1]/label[1]/span[2]/text()")) == 0:
+            item['question_num'] = ""
+        item['question_num'] = response.xpath("/html/body/div[1]/div[2]/div[2]/div[1]/div[1]/label[1]/span[2]/text()").extract()[0]
+
+        if len(response.xpath("/html/body/div[1]/div[2]/div[1]/a[3]/text()"))  == 0:
+            item['question_type'] = ""
+        item['question_type'] = response.xpath("/html/body/div[1]/div[2]/div[1]/a[3]/text()").extract()[0]
+
+        if len(response.xpath("/html/body/div[1]/div[2]/div[2]/div[1]/div[1]/label[2]/span[2]/text()")) == 0:
+            item['difficulty_level'] = ""
+        item['difficulty_level'] = response.xpath("/html/body/div[1]/div[2]/div[2]/div[1]/div[1]/label[2]/span[2]/text()").extract()[0]
+
+        if len(response.xpath("/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/div/text()")) == 0:
+            item['question_item'] = ""
+        item['question_value'] = response.xpath("/html/body/div[1]/div[2]/div[2]/div[1]/div[2]/div/text()").extract()
+
+        if len(response.xpath("//*[@id='i-tab-content']/div/text()")) == 0:
+            item['answer_value'] = ""
+        item['answer_value'] = response.xpath("//*[@id='i-tab-content']/div/text()").extract()[0]
+        
+        if len(response.xpath("//*[@id='i-tab-content1']/div/text()")): 
+            if len(response.xpath("/html/body/div[1]/div[2]/div[2]/div[4]/div[2]/a[2]/@href")) == 0:
+                 item['answer_extend_link'] = ""
+            item['answer_extend_link'] = self.base_url + response.xpath("/html/body/div[1]/div[2]/div[2]/div[4]/div[2]/a[2]/@href").extract()[0] + self.end_url
+            item['answer_explain'] = response.xpath("//*[@id='i-tab-content1']/div/text()").extract()[0]
+        else:
+            if len(response.xpath("/html/body/div[1]/div[2]/div[2]/div[3]/div[2]/a[2]/@href")) == 0:
+                 item['answer_extend_link'] = ""
+            item['answer_extend_link'] = self.base_url + response.xpath("/html/body/div[1]/div[2]/div[2]/div[3]/div[2]/a[2]/@href").extract()[0] + self.end_url
+            item['answer_explain'] = ""
+        
+        print(item)
+        # yield item
+
+
+
+       
